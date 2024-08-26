d474249ff4e5c59cc94aa280168d6fcd497563e9
jignesh119 <jignesh3503@gmail.com>
2024-08-27 00:20:07 +0530

    setup homepage of app, routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import EditorPage from "./pages/EditorPage";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor/:editorId" element={<EditorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,73 @@
+import React, { KeyboardEvent, KeyboardEventHandler, useState } from "react";
+
+const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const id = Math.random().toString();
+    setRoomId(id);
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      return;
+    }
+
+    // Redirect
+    //redirect to editor page with username state
+  };
+
+  const handleInputEnter = (e: KeyboardEvent) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with 💛 &nbsp; by &nbsp;
+          <a href="https://github.com/jignesh119">Jignesh119</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
